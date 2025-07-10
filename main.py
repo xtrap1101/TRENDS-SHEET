067d2e55898bf824ccdf6f1d92f512a0add4e176
Ano Anoo <135571466+xtrap1101@users.noreply.github.com>
2025-07-10 21:39:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -117,3 +117,4 @@ if __name__ == "__main__":
     # Render cung cấp cổng (PORT) qua một biến môi trường
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
+# ĐẤNG TỐI CAO SOI SÁNG ĐOẠN CODE AI NÀY HUHU
